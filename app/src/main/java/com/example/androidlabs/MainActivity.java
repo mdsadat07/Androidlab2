9e1d47f025fc591d3d9d0a1ba85a39419566239d
mdsadat07 <md.sadat07@gmail.com>
2024-10-05 16:54:47 -0400

    lab_2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.androidlabs;
-
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button pressMeButton;
+    private EditText editText;
+    private CheckBox checkBox;
+    private Switch switch1;
+    private ImageButton flagButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main_constraint);  // Set the layout to Constraint Layout
+
+        textView = findViewById(R.id.textView);
+        pressMeButton = findViewById(R.id.pressMeButton);
+        editText = findViewById(R.id.editText);
+        checkBox = findViewById(R.id.checkBox);
+        switch1 = findViewById(R.id.switch1);
+        flagButton = findViewById(R.id.flagButton);
+
+        // Button onClickListener
+        pressMeButton.setOnClickListener(v -> {
+            String enteredText = editText.getText().toString();
+            textView.setText(enteredText);
+
+            // Show Toast message
+            Toast.makeText(MainActivity.this, getString(R.string.toast_message), Toast.LENGTH_SHORT).show();
+        });
+
+        // CheckBox onCheckedChangeListener
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            String state = isChecked ? getString(R.string.on) : getString(R.string.off);
+            String snackMessage = getString(R.string.checkbox_state, state);
+            Snackbar snackbar = Snackbar.make(buttonView, snackMessage, Snackbar.LENGTH_LONG);
+
+            // Undo action
+            snackbar.setAction(getString(R.string.undo), v -> checkBox.setChecked(!isChecked));
+            snackbar.show();
         });
     }
 }
