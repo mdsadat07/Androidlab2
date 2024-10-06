511056a4587abd51d023823d81477c039d6c4346
mdsadat07 <md.sadat07@gmail.com>
2024-10-05 23:04:36 -0400

    lab4
    Signed-off-by: mdsadat07 <md.sadat07@gmail.com>

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,24 +1,100 @@
 package com.example.androidlabs;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<TodoItem> todoList = new ArrayList<>();
+    TodoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ListView listView = findViewById(R.id.listView);
+        EditText editText = findViewById(R.id.editText);
+        Switch urgentSwitch = findViewById(R.id.urgentSwitch);
+        Button addButton = findViewById(R.id.addButton);
+
+        adapter = new TodoAdapter();
+        listView.setAdapter(adapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String task = editText.getText().toString();
+                boolean isUrgent = urgentSwitch.isChecked();
+                todoList.add(new TodoItem(task, isUrgent));
+                editText.setText(""); // Clear the EditText
+                adapter.notifyDataSetChanged();
+            }
         });
+
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Do you want to delete this?")
+                    .setMessage("The selected row is: " + position)
+                    .setPositiveButton("Delete", (dialog, which) -> {
+                        todoList.remove(position);
+                        adapter.notifyDataSetChanged();
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+            return true;
+        });
+    }
+
+    class TodoAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return todoList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return todoList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_item, parent, false);
+            }
+
+            TodoItem currentItem = todoList.get(position);
+            TextView todoText = convertView.findViewById(R.id.todoText);
+            todoText.setText(currentItem.getText());
+
+            if (currentItem.isUrgent()) {
+                convertView.setBackgroundColor(Color.RED);
+                todoText.setTextColor(Color.WHITE);
+            } else {
+                convertView.setBackgroundColor(Color.WHITE);
+                todoText.setTextColor(Color.BLACK);
+            }
+
+            return convertView;
+        }
     }
 }
